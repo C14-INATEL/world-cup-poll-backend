3c9bc32e0300529aeb84a2836b81f4c0a0ca6c93
joaolucenalima <joaovitorlucena000@gmail.com>
2026-03-26 09:39:47 -0300

    feat: add seed games script to populate game table data

---
+++ src/scripts/seed-games.ts
@@ -0,0 +1,20 @@
+import { getAllMatchesFromApiJob } from '@/jobs/get-games.job'
+import logger from '@/logger'
+
+async function main() {
+	logger.info('[SEED] Starting games seed by calling getAllMatchesFromApiJob')
+
+	try {
+		await getAllMatchesFromApiJob()
+		logger.info('[SEED] Games seed finished successfully')
+	} catch (error) {
+		logger.error({
+			message: '[SEED] Games seed failed',
+			error: (error as Error).message,
+			stack: (error as Error).stack,
+		})
+		process.exitCode = 1
+	}
+}
+
+void main()
